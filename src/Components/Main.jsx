44b6f0e68e1bdc7dd3fa861e39e91411378b6879
meera <shu@ahb.com>
2021-07-15 17:06:51 +0530

    Projects

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,15 +3,18 @@ import About from './About/About'
 import Contact from './Contact/Contact'
 import Intro from './Intro/Intro'
 import Navbar from './Navbar/Navbar'
+import Project from './Project/Project'
 import Skills from './Skills/Skills'
 
 const Main = () => {
   return (
+    
     <div>
       <Navbar/>
       <Intro/>
       <About/>
       <Skills/>
+      <Project/>
       <Contact/>
     </div>
   )
