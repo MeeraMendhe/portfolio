89d1545832a4b6fedbea8f1f047244463f8a1f9a
meera <shu@ahb.com>
2021-07-16 20:49:15 +0530

    contact_send to mail

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
 import styled from "./Contact.module.css"
+import emailjs from "emailjs-com";
 const Contact = () => {
+  const handleSubmit=(e)=>
+  {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        "service_f3322fh",
+        "template_livi7up",
+        e.target,
+        "user_9LffP4jCfFdSRK43r6Mcn"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
-    <div id="contact">
+    <form onSubmit={handleSubmit} id="contact">
       <h1 className={styled.h1}>Contact Form</h1>
       <div>
-      <input className={styled.color} placeholder="First Name" type="text"/>
-      <input className={styled.color1} placeholder="Full Name" type="text"/>
+      <input className={styled.color} name="user_name" placeholder="Full Name" type="text" required/>
+      <input className={styled.color1} name="contact_number" placeholder="Mobile Number" type="text" required/>
       </div>
       <div>
-      <input className={styled.color2} placeholder="Email Id" type="text"/>
+      <input className={styled.color2} name="user_email" placeholder="Email Id" type="email" required/>
       </div>
       <div>
-      <input className={styled.color2} placeholder="Subject of the message" type="text"/>
+      <input className={styled.color2} name="message" placeholder="Subject of the message" type="text" required/>
       </div>
-      <div>
-      <input className={styled.color3} placeholder="Type your message herex" type="text"/>
-      </div>
-      <button className={styled.btn}>Send Message</button>
-    </div>
+      <button type="submit" className={styled.btn}>Send Message</button>
+    </form>
   )
 }
 
