fabd5ee0cc6cb0d6fcf4ae6fd853d787d5b00cf3
meera <shu@ahb.com>
2021-07-19 12:40:56 +0530

    link attached

--- src/Components/Sidebar/SideBar.jsx
+++ src/Components/Sidebar/SideBar.jsx
@@ -4,10 +4,29 @@ import styled from './SideBar.module.css'
 const SideBar = () => {
   return (
     <div className={styled.fix}>
-      <AiFillGithub className={styled.color1}/><br/>
-      <AiFillTwitterCircle className={styled.color}/><br/>
-      <AiFillLinkedin className={styled.color}/><br/>
-      <AiFillFacebook className={styled.color}/><br/>
+          <a
+              className={styled.color3}
+              href="https://github.com/MeeraMendhe"
+            >
+              <AiFillGithub className={styled.color1}/><br/>
+            </a>
+            <a
+              className={styled.color3}
+              href="https://twitter.com/home?lang=en"
+            >
+              <AiFillTwitterCircle className={styled.color}/><br/>
+            </a>     <a
+              className={styled.color3}
+              href="https://www.linkedin.com/in/meera-mendhe-953240201/"
+            >
+              <AiFillLinkedin className={styled.color}/><br/>
+            </a>     <a
+              className={styled.color3}
+              href="https://www.facebook.com/meera.mendhe"
+            >
+              <AiFillFacebook className={styled.color}/><br/>
+            </a>
+    
     </div>
   )
 }
