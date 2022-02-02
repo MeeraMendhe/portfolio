a834415cf0466541734a3774b73e1fc2c1d512ff
meera <shu@ahb.com>
2022-02-02 20:05:49 +0530

    upload new image

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import styled from "./Resume.module.css"
-import pdf from '../files/my.pdf'
 const Resume = () => {
   return (
     <div id="resume">
       <h1 className={styled.mid}>Resume</h1>
       <div  className={styled.flex}>
       <img className={styled.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF01H2L8h62qzU9Fykx2hig89U0Q73-EwVug&usqp=CAU" alt="cv"/>
-      <a href={pdf} download><button className={styled.btn}>See My Resume</button></a>
+      <a href="https://drive.google.com/drive/folders/1kwqNvdg2RL4U6Q01CyZ-ULo9Cq-gv-wH?usp=sharing" download><button className={styled.btn}>See My Resume</button></a>
       </div>
      
     </div>
